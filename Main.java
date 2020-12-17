6a272a45c709c66aa942bbf87048e33250edf796
Julia81-pr <75021260+Julia81-pr@users.noreply.github.com>
2020-12-17 10:23:32 +0300

    Вторая задача
    Убран service-класс
    Добавлены условия при пополнении баланса на сумму меньше 1000

---
+++ Main.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int balance = 100; // начальный баланс клиента
+        System.out.println("Ваша сумма: ");
+        int summa = input.nextInt(); // вводим сумму пополнения
+        int balance2 = balance+summa; //баланс после пополнения
+        int bonus = summa/100; // расчет бонуса за каждые 100 руб
+        if(summa >=1000){
+            System.out.println(" Вам начислен бонус: "+ bonus);
+            System.out.println(" Ваш текущий баланс: "+ balance2);
+        }
+
+        if(summa <1000){
+            System.out.println(" Ваш баланс: "+ balance2);
+        }
+
+
+
+
+
+    }
+}
